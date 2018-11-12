1ff34ca2451f81efe6e9f516eac5fc0201c83dae
huangchengke <huangchengke@fkhwl.com>
2018-11-12 16:28:20 +0800

    测试maven库提交

--- app/src/main/java/lib/hck/com/android_libs/MainActivity.java
+++ app/src/main/java/lib/hck/com/android_libs/MainActivity.java
@@ -3,10 +3,8 @@ package lib.hck.com.android_libs;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.hck.lib.picter.Utils;
 
 public class MainActivity extends Activity {
-   private int a=100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
