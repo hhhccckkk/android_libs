244bbbfb1ed69d0335a89eb8f89a5fcd3e83ddbc
huangchengke <huangchengke@fkhwl.com>
2018-11-12 16:04:39 +0800

    测试maven库提交

--- app/src/main/java/lib/hck/com/android_libs/MainActivity.java
+++ app/src/main/java/lib/hck/com/android_libs/MainActivity.java
@@ -3,11 +3,14 @@ package lib.hck.com.android_libs;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.hck.lib.picter.Utils;
+
 public class MainActivity extends Activity {
    private int a=100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Utils.log("哈哈哈啊哈哈");
     }
 }
---
+++ hcklib/src/main/java/com/hck/lib/picter/Utils.java
@@ -0,0 +1,14 @@
+package com.hck.lib.picter;
+
+import android.util.Log;
+
+/**
+ * @author ：
+ * @date：2018/11/12
+ */
+
+public class Utils {
+    public static void log(String data) {
+        Log.d("hck", data);
+    }
+}
